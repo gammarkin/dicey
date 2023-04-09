3db0fae620c0ad328c0447c509bce108f477f3d5
gammarkin <marcosrbnsf@gmail.com>
2023-04-09 11:52:05 -0300

    refactor: changes interface to follow new model pattern

--- api/src/interfaces/IChar.ts
+++ api/src/interfaces/IChar.ts
@@ -2,7 +2,7 @@ export default interface IChar {
 	id: number;
 	playerTag: string;
 	characterName: string;
-	skills: object;
-	attributes: object;
-	weapons: [] | undefined;
+	skills: [];
+	attributes: [];
+	weapons: [];
 }
