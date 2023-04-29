f44394252504e032e5b1e2f36ff7013799549aa5
gammarkin <marcosrbnsf@gmail.com>
2023-04-28 21:54:47 -0300

    fix: fixes modifier bug

---
+++ bot/src/commands/rollDice.js
@@ -0,0 +1,53 @@
+const getUserTag = require('../helpers/getUserTag');
+const randomInteger = require('random-integer')
+
+const { BOT_USER_TAG } = process.env;
+
+module.exports = async (message) => {
+  const [user, userTag] = getUserTag(message);
+
+  if (message.content.includes('!r') && userTag !== BOT_USER_TAG) {
+    if (message.content.split(' ').length < 2)
+      return message.reply('formatação errada! Tente !roll 1d20 +(modificador)');
+
+    const rollsAndSides = message.content.split(' ')[1].replace('d', ' ').split(' ')
+    const dice = message.content.split(' ')[1]
+    const numberOfRolls = Number(rollsAndSides[0])
+    const sides = Number(rollsAndSides[1])
+
+    let mod = 0;
+    let biggestRoll = 0;
+
+    if (message.content.includes('+')) {
+      mod = Number(message.content.split(' ')[2].replace('+', ''));
+
+      if (isNaN(mod)) return message.reply('o modificador deve ser um número!');
+    }
+
+    const MIN_DICE_VALUE = 1;
+    const rolls = [];
+
+    for (let i = 0; i < numberOfRolls; i++) {
+      const value = randomInteger(MIN_DICE_VALUE, sides + MIN_DICE_VALUE) + mod;
+
+      rolls.push({ name: `roll #${i}`, value, inline: true });
+
+      if (value > biggestRoll) biggestRoll = value;
+    }
+
+    const total = rolls.reduce((acc, curr) => acc + curr.value, 0);
+
+    return message.channel.send({
+      embeds: [{
+        color: 0xf54257,
+        title: `Maior dado: ${biggestRoll} | Total: ${total}`,
+        description: `player: ${user} | dado: ${dice} | modificador: ${mod}`,
+        fields: rolls,
+        timestamp: new Date().toISOString(),
+        footer: {
+          text: `foi adicionado ${mod} ao seu dado`,
+        },
+      }]
+    });
+  }
+}
