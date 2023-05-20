96bb232a96ddc5c21b048d258ea822e1f8e126ee
gammarkin <marcosrbnsf@gmail.com>
2023-05-20 18:28:25 -0300

    feat: adds routes login and register

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-const router = createBrowserRouter([{path: '/', element: <App />}]);
+const router = createBrowserRouter([
+	{path: '/', element: <App />},
+	{path: '/login', element: <Login />},
+	{path: '/register', element: <Register />},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
